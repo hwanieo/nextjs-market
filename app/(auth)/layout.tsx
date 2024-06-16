1d5c4e00e99ca6a682a892303f37fd7edf174181
hwnii <hwniieo@gmail.com>
2024-06-16 21:34:23 +0900

    로그인 회원가입 페이지

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,7 @@
-export default function Auth() {
-  return <div>layout</div>;
+export default function Auth({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-[calc(100vh-62px)]  items-center justify-center">
+      {children}
+    </div>
+  );
 }
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,40 @@
 export default function Page() {
-  return <div>login page</div>;
+  return (
+    <form className="flex flex-col gap-4 w-full max-w-sm mb-40 sm:">
+      <div>
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="mt-1  w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          required
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="password"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="w-full px-4 py-2 bg-slate-600 text-white rounded-md shadow-sm hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        로그인
+      </button>
+    </form>
+  );
 }
